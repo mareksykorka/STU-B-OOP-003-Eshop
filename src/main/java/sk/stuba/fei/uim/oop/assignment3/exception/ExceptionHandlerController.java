391c3c3bfc88779cd79508a35cbe2c4d365e972f
Marek Sýkorka <marcopolo1234565@gmail.com>
2023-05-03 08:59:18 +0200

    Fix - v1.3.1  ~ Exception Fix

--- src/main/java/sk/stuba/fei/uim/oop/assignment3/exception/ExceptionHandlerController.java
+++ src/main/java/sk/stuba/fei/uim/oop/assignment3/exception/ExceptionHandlerController.java
@@ -1,6 +1,5 @@
 package sk.stuba.fei.uim.oop.assignment3.exception;
 
-import javassist.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
